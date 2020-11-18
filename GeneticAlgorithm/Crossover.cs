614b101de8868fe5be76a2981bc5d681fe25b6a1
Tomasz Steblik <tomek@localhost.localdomain>
2020-11-18 02:44:03 +0100

    clean up

--- GeneticAlgorithm/Crossover.cs
+++ GeneticAlgorithm/Crossover.cs
@@ -95,4 +95,85 @@ namespace GeneticAlgorithm
             return offsprings;
         }
     }
+
+    public class HGreXCrossover : Crossover
+    {
+        protected override int[] GenerateOffspring(int[] parent1, int[] parent2)
+        {
+            var length = parent1.Length;
+            var offspring = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                offspring[i] = -1;
+            }
+            int[] currentParent;
+            int[] nextParent;
+            if (Random.Next(0, 1) == 1)
+            {
+                currentParent = parent1;
+                nextParent = parent2;
+            }
+            else
+            {
+                currentParent = parent2;
+                nextParent = parent1;
+            }
+            var count = -1;
+            offspring[0] = currentParent[0];
+            count++;
+            offspring[1] = currentParent[1];
+            count++;
+
+            while (count < length-1)
+            {
+                int indexInCurrent;
+                int indexInNext;
+                
+                //sprawdzenie indeksow czy nie sa spoza puli oraz obliczenie odleglosci
+                int nextParentDistanceToNextAlle =3;
+                int currentParentDistanceToNextAlle = 3;
+
+                if (nextParentDistanceToNextAlle<currentParentDistanceToNextAlle)
+                {
+                    int nextAlle = 3;
+                    if (!Helper.IsThereGene(offspring,nextAlle))
+                    {
+                        //wymiana
+                    }
+                    else
+                    {
+                        //losowanko
+                    }
+                }
+                else
+                {
+                    int nextAlle = 3;
+                    if (!Helper.IsThereGene(offspring,nextAlle))
+                    {
+                        //wymiana
+                    }
+                    else
+                    {
+                        //losowanko
+                    }
+                }
+                count++;
+            }
+
+            return offspring;
+        }
+
+        public override int[][] GenerateOffsprings(int[][] parents)
+        {
+            var parentsLength = parents.Length;
+            var amountOfChildren = parentsLength / 2;
+            int[][] offsprings = new int[amountOfChildren][];
+            
+            for (int c=0,j = 0; c < amountOfChildren; j+=2,c++)
+            {
+                offsprings[c] = GenerateOffspring(parents[j], parents[j + 1]);
+            }
+            return offsprings;
+        }
+    }
 }
