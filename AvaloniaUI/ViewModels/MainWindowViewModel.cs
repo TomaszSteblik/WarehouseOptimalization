04587d2d9b8b22aab8317922c4fc33bf83cc1db6
rtrydev <r.kulka44@gmail.com>
2021-06-24 09:28:59 +0200

    brakujace pliki

---
+++ AvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public DistanceViewModel DistanceViewModel { get; set; } = new DistanceViewModel();
+        public WarehouseViewModel WarehouseViewModel { get; set; } = new WarehouseViewModel();
+    }
+}
