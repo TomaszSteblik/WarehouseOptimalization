62d847358d466e971894811324353a44c59868b9
rtrydev <r.kulka44@gmail.com>
2021-12-12 19:16:18 +0100

    add avalonia ui project

---
+++ Runner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Runner.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
