1413c98188d546aef352ffb4b3c8357ab5b52327
rtrydev <r.kulka44@gmail.com>
2021-12-15 21:08:09 +0100

    add basic param combobox

--- Runner/Models/ParametersModel.cs
+++ Runner/Models/ParametersModel.cs
@@ -1,6 +1,15 @@
+using Optimization.GeneticAlgorithms.Crossovers;
+using Optimization.GeneticAlgorithms.Eliminations;
+using Optimization.GeneticAlgorithms.Mutations;
+using Optimization.GeneticAlgorithms.Selections;
+
 namespace Runner.Models;
 
 public class ParametersModel
 {
     public string[] SelectedFiles { get; set; }
+    public SelectionMethod SelectedSelection { get; set; } = SelectionMethod.RouletteWheel;
+    public CrossoverMethod SelectedCrossover { get; set; } = CrossoverMethod.HProX;
+    public EliminationMethod SelectedElimination { get; set; } = EliminationMethod.Elitism;
+    public MutationMethod SelectedMutation { get; set; } = MutationMethod.RSM;
 }
--- Runner/ViewModels/ParametersViewModel.cs
+++ Runner/ViewModels/ParametersViewModel.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
+using Optimization.GeneticAlgorithms.Crossovers;
+using Optimization.GeneticAlgorithms.Eliminations;
+using Optimization.GeneticAlgorithms.Mutations;
+using Optimization.GeneticAlgorithms.Selections;
 using ReactiveUI.Fody.Helpers;
 using Runner.Commands;
 using Runner.Models;
@@ -7,9 +14,15 @@ namespace Runner.ViewModels;
 
 public class ParametersViewModel : ViewModelBase
 {
+    
+    public List<SelectionMethod> Selections { get; set; } = Enum.GetValues(typeof(SelectionMethod)).Cast<SelectionMethod>().ToList();
+    public List<CrossoverMethod> Crossovers { get; set; } = Enum.GetValues(typeof(CrossoverMethod)).Cast<CrossoverMethod>().ToList();
+    public List<EliminationMethod> Eliminations { get; set; } = Enum.GetValues(typeof(EliminationMethod)).Cast<EliminationMethod>().ToList();
+    public List<MutationMethod> Mutations { get; set; } = Enum.GetValues(typeof(MutationMethod)).Cast<MutationMethod>().ToList();
     public ICommand SelectData { get; set; }
     public ParametersModel ParametersModel { get; set; }
     [Reactive] public string SelectedFilesString { get; set; }
+    
     public ParametersViewModel(ParametersModel model)
     {
         ParametersModel = model;
